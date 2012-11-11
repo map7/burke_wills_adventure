7097b2dae08985e752ce446e6f9a313e5472bb71
Michael Pope <map7@corsairsolutions.com.au>
2012-11-11 22:45:25 +1100

    initial git

---
+++ tute1.rb
@@ -0,0 +1,22 @@
+require 'gosu'
+
+class GameWindow < Gosu::Window
+  def initialize
+    super 1024,768, false
+    self.caption = "Burke & Wills Bogus Adventure"
+  end
+  
+  def update
+    # Main game logic
+    # 60 times p/sec
+    # window, image, tileable
+    @background_image = Gosu::Image.new(self, "background.png", true)
+  end
+  
+  def draw
+    @background_image.draw(0,0,0)
+  end
+end
+
+window = GameWindow.new
+window.show
