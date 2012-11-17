947283c7492292f827114b84031c8716c51b0bff
Aaron Gitlin <aarongitlin@gmail.com>
2012-11-17 22:48:59 +1100

    message text!

--- bw.rb
+++ bw.rb
@@ -21,7 +21,6 @@ class GameWindow < Gosu::Window
 
     # Load default font and play little sound to start game
     @font = Gosu::Font.new(self, Gosu::default_font_name, 20)
-    @speech_font = Gosu::Font.new(self, Gosu::default_font_name, 20)
     @beep = Gosu::Sample.new(self, "media/sounds/Pickup-coin.wav")
     @beep.play
 
@@ -73,12 +72,10 @@ class GameWindow < Gosu::Window
     @font.draw("Burke & Wills Bogus Adventure", 10,10, 1)
     @font.draw("Health #{@player.health}", WIDTH - 220,10, 1)        
     @font.draw("Score #{@player.score}", WIDTH - 120,10, 1)
-    @font.draw("Oh no! I've lost Wills! I need to find him if we're ever"+
-                "going to make it back to camp in time!",
-                250, HEIGHT-120, 2,
-                color=0xffffffff
-                )
-    
+   
+    @font.draw("Oh no! I've lost Wills! I need to find him if we're ever",
+                350, HEIGHT-120, 2, 1, 1, 0xff000000)
+    @font.draw("going to make it back to camp in time!", 350, HEIGHT-90, 2, 1, 1, 0xff000000)
 
     @terrain.draw
     @enemies.each {|enemy| enemy.draw }
