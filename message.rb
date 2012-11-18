fc22f3576b320a8a2ccc7b3a699bab0ec4f11d21
Michael Pope <map7@corsairsolutions.com.au>
2012-11-18 17:28:21 +1100

    Allow message to change between stages

--- message.rb
+++ message.rb
@@ -9,14 +9,13 @@ class Message
   def initialize(window)
     @message = Gosu::Image.new($window, "media/images/bottom_screen_dialog.png",true)
     @show = true
+    @text = ""
   end
   
   def draw
     if self.show
-      @text = "Oh no! I've lost Wills! I need to find him if we're evergoing to make it back to camp in time!"
-
-      @text = Chingu::Text.new(@text, x: 350, y: HEIGHT-120, zorder: 55, factor_x: 1.0, factor_y: 1.0, color: 0xff000000, max_width: 500, size: 20, font: "media/fonts/pixelated.ttf")
-      @text.draw
+      @message_text = Chingu::Text.new(self.text, x: 350, y: HEIGHT-120, zorder: 55, factor_x: 1.0, factor_y: 1.0, color: 0xff000000, max_width: 500, size: 20, font: "media/fonts/pixelated.ttf")
+      @message_text.draw
       @message.draw(0, HEIGHT-180, 1)
     end
   end
--- stage1.rb
+++ stage1.rb
@@ -4,5 +4,6 @@ class Stage1 < Stage
     @current_stage = 1
     @bg_image = Image["media/images/desert_tile.png"]    # Set background image
     @enemies = (rand(MAX_ENEMIES)).times.map{|i| Enemy.new(self, 0)}    # Create enemy's
+    @message.text = "Oh no! I've lost Wills! I need to find him if we're evergoing to make it back to camp in time!"
   end
 end
