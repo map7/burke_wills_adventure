3d69198a81f05e4c41af8cd26d1213099bf0f063
Aaron Gitlin <aarongitlin@gmail.com>
2012-11-18 17:37:23 +1100

    trying to add berries to bushes

--- terrain.rb
+++ terrain.rb
@@ -4,11 +4,24 @@
 
 class Terrain
 
-  def initialize(window)
-    @shrub = Image["media/images/shrub.png"]
+  def initialize(window, shrub)
     @locations = []
     rand(20).times do
       @locations << {x: rand(1024), y: rand(768)}
+    end
+    @shrubs = Gosu::Image.load_tiles($window, "media/images/shrub_sprite_50.png", 50, 48, false)
+    @shrub = @shrubs[0]
+    
+    case shrub
+    when 0
+      @shrub = @shrubs[0]
+    when 1
+      @shrub = @shrubs[1]
+      @color = "red"
+    when 2
+      @shrub = @shrubs[2]
+      @color = "purple"
+    when 3
     end
   end
 
