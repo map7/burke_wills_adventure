1cff8b24559c15afc0c44e31b50742e6289fed9e
Michael Pope <map7@corsairsolutions.com.au>
2012-11-18 17:22:55 +1100

    Refactor the font in message

--- message.rb
+++ message.rb
@@ -8,16 +8,15 @@ class Message
 
   def initialize(window)
     @message = Gosu::Image.new($window, "media/images/bottom_screen_dialog.png",true)
-    @font = Font["media/fonts/pixelated.ttf", 20]
     @show = true
   end
   
   def draw
     if self.show
-      @font.draw("Oh no! I've lost Wills! I need to find him",
-                 350, HEIGHT-120, 2, 1, 1, 0xff000000)
-      @font.draw("if we're evergoing to make it back", 350, HEIGHT-90, 2, 1, 1, 0xff000000)
-      @font.draw("to camp in time!", 350, HEIGHT-60, 2, 1, 1, 0xff000000)
+      @text = "Oh no! I've lost Wills! I need to find him if we're evergoing to make it back to camp in time!"
+
+      @text = Chingu::Text.new(@text, x: 350, y: HEIGHT-120, zorder: 55, factor_x: 1.0, factor_y: 1.0, color: 0xff000000, max_width: 500, size: 20, font: "media/fonts/pixelated.ttf")
+      @text.draw
       @message.draw(0, HEIGHT-180, 1)
     end
   end
